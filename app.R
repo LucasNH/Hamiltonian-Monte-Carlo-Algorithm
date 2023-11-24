383f1b0baa2620778b54cc9e178c4264037ce1d5
Anna Ly <annaly@Annas-MacBook-Air.local>
2023-11-24 12:53:23 -0500

    Added some comments for documentation; planning to revise the HMC function such that we don't need to generate outside of it.

--- app.R
+++ app.R
@@ -1,12 +1,10 @@
-# here: want to make an R Shiny site based on the algorithm
 library(shiny)
 library(shinycssloaders)
-# below: codes for the project
-
 options(spinner.color="#9dabf5", spinner.color.background="#ffffff", type = 6)
 
+# This is the front-end - user inputs and what are displayed.
 ui = fluidPage(
-
+  
   titlePanel("Hamiltonian Monte Carlo"),
 
   sidebarLayout(
@@ -18,47 +16,46 @@ ui = fluidPage(
                                  "Normal" = "norm")),
       conditionalPanel(
         condition = "input.graph_type == 'beta'",
-        numericInput(inputId = "alpha_prime", label = "alpha parameter",
-                     value = 50),
-        numericInput(inputId = "beta_prime", label = "beta parameter",
-                     value = 50),
+        numericInput(inputId = "alpha_prime", label = "alpha parameter", value = 50),
+        numericInput(inputId = "beta_prime", label = "beta parameter", value = 50),
       ),
       
       conditionalPanel(
         condition = "input.graph_type == 'norm'",
-        numericInput(inputId = "mu_mean", label = "mu parameter",
-                     value = 0),
-        numericInput(inputId = "sigma_sd", label = "sigma parameter",
-                     value = 1),
+        numericInput(inputId = "mu_mean", label = "mu parameter", value = 0),
+        numericInput(inputId = "sigma_sd", label = "sigma parameter", value = 1),
       ),
       
-      numericInput(inputId = "bigN", label = "The number of samples", 
-                   value = 5000, min = 1),
+      numericInput(inputId = "bigN", label = "The number of samples", value = 5000, min = 1),
       numericInput(inputId = "current_position", label = "The current position of your sample", 
                    value = 0.3),
       numericInput(inputId = "stepsize", label = "How long are you stepping to the next position?",
                    value = 0.01),
-      p("Remark: for normal, use 0.2 for a reasonable result."),
-      numericInput(inputId = "num_steps", label = "How many steps are you taking?",
-                   value = 2),
+      strong("Remarks"),
+      p("For for beta, 0.01 leads to a reasonable result."),
+      p("For normal, use 0.2 leads to a reasonable result."),
+      numericInput(inputId = "num_steps", label = "How many steps are you taking?",value = 2),
       
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
-      
       # below the textoutput is a placeholder
       #verbatimTextOutput(outputId = "HMC_results")
       withSpinner(plotOutput(outputId = "HMC_plot"), type = 6),
-      
     )
   )
 )
 
-# Define server logic required to draw a histogram ----
+# This is the back-end.
 server = function(input, output) {
 
   grad_U = function(q, type, alpha = 50, beta = 50){
+    # U represents a distribution, and grad_U is the gradient of U.
+    # q: represents a sample from the distribution of U.
+    # type: the type of distribution the user is sampling from.
+    # alpha: the alpha parameter if the user samples from a beta dist.
+    # beta: the beta parameter if the user samples from a beta dist.
     if(type == "beta"){
       return(-(alpha/q - beta/(1-q)))
     } else if (type == "norm"){
@@ -67,15 +64,16 @@ server = function(input, output) {
   }
   
   HMC = function(U_type, epsilon, L, current_q,
-                 alpha = 50, beta = 50,
-                 mu = 0, sigma = 1){
-    # note: parameters past current_q are inputs for the potential energies depending on the
-    # distribution the user wants to utilize
+                 alpha = 50, beta = 50, mu = 0, sigma = 1){
+    # This function simulates the HMC once. You need to iterate over it for multiple samples.
+    # U_type: the type of distribution we are sampling from. Here, our kinetic energy 
+    # in this code, q represents a sample from the posterior distribution of interest...
     q = current_q
-    p = rnorm(length(q),0,1) # independent standard normal variates
+    # Here, our kinetic energy is a standard normal distribution.
+    p = rnorm(n = 1, mean = 0, sd = 1) 
     current_p = p
     ######################################################
-    # Leapfrog Algorithm 
+    # Leapfrog Algorithm for solving the dynamical system
     # Make a half step for momentum at the beginning
     p = p - epsilon * grad_U(q, U_type, alpha, beta) / 2
     # Alternate full steps for position and momentum
@@ -89,7 +87,6 @@ server = function(input, output) {
     p = p - epsilon * grad_U(q, U_type, alpha, beta) / 2
     ######################################################
     # Evaluate potential and kinetic energies at start and end of trajectory
-    # switches depending on the value of U
     if(U_type == "beta"){
       current_U = -dbeta(current_q, alpha, beta, log = TRUE)
       proposed_U = -dbeta(q, alpha, beta, log = TRUE)
@@ -97,16 +94,13 @@ server = function(input, output) {
       current_U = -dnorm(current_q, mu, sigma, log = TRUE)
       proposed_U = -dnorm(q, mu, sigma, log = TRUE)
     }
-    current_K = sum(current_p^2) / 2
-    proposed_K = sum(p^2) / 2
-    # Accept or reject the state at end of trajectory, returning either
-    # the position at the end of the trajectory or the initial position
-    if (runif(1) < exp(current_U-proposed_U+current_K-proposed_K)){
-      return (q) # accept
-    }
-    else{
-      return (current_q) # reject
-    }
+    current_K = sum(current_p^2)/2
+    proposed_K = sum(p^2)/2
+    # This part corresponds with the acceptance-rejection method.
+    if (runif(1) < exp(current_U-proposed_U+current_K-proposed_K))
+      { return (q) } # accept
+    else 
+      { return (current_q) } # reject
   }
   
   
