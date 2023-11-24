06714ff23325edee9aa73d35e2224d8f272adb7e
AnnaHuynhLy <annahuynhly012920@gmail.com>
2023-11-23 22:45:16 -0500

    incorporated the normal distribution

--- app.R
+++ app.R
@@ -11,16 +11,34 @@ ui = fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      numericInput(inputId = "alpha_prime", label = "alpha parameter",
-                  min = 1, value = 50),
-      numericInput(inputId = "beta_prime", label = "beta parameter",
-                  min = 1, value = 50),
+      selectInput(inputId = "graph_type", 
+                  label = "select the type of distribution to sample from",
+                  selected = "beta", 
+                  choices = list("Beta" = "beta",
+                                 "Normal" = "norm")),
+      conditionalPanel(
+        condition = "input.graph_type == 'beta'",
+        numericInput(inputId = "alpha_prime", label = "alpha parameter",
+                     value = 50),
+        numericInput(inputId = "beta_prime", label = "beta parameter",
+                     value = 50),
+      ),
+      
+      conditionalPanel(
+        condition = "input.graph_type == 'norm'",
+        numericInput(inputId = "mu_mean", label = "mu parameter",
+                     value = 0),
+        numericInput(inputId = "sigma_sd", label = "sigma parameter",
+                     value = 1),
+      ),
+      
       numericInput(inputId = "bigN", label = "The number of samples", 
                    value = 5000, min = 1),
       numericInput(inputId = "current_position", label = "The current position of your sample", 
                    value = 0.3),
       numericInput(inputId = "stepsize", label = "How long are you stepping to the next position?",
                    value = 0.01),
+      p("Remark: for normal, use 0.2 for a reasonable result."),
       numericInput(inputId = "num_steps", label = "How many steps are you taking?",
                    value = 2),
       
@@ -38,38 +56,48 @@ ui = fluidPage(
 )
 
 # Define server logic required to draw a histogram ----
-server <- function(input, output) {
+server = function(input, output) {
 
-  U = function(q){
-    return(-dbeta(q, input$alpha_prime, input$beta_prime, log = TRUE))
+  grad_U = function(q, type, alpha = 50, beta = 50){
+    if(type == "beta"){
+      return(-(alpha/q - beta/(1-q)))
+    } else if (type == "norm"){
+      return(q)
+    }
   }
   
-  grad_U = function(q){
-    return(-(input$alpha_prime/q - input$beta_prime/(1-q)))
-  }
-  
-  HMC = function(U, grad_U, epsilon, L, current_q){
+  HMC = function(U_type, epsilon, L, current_q,
+                 alpha = 50, beta = 50,
+                 mu = 0, sigma = 1){
+    # note: parameters past current_q are inputs for the potential energies depending on the
+    # distribution the user wants to utilize
     q = current_q
     p = rnorm(length(q),0,1) # independent standard normal variates
     current_p = p
     ######################################################
     # Leapfrog Algorithm 
     # Make a half step for momentum at the beginning
-    p = p - epsilon * grad_U(q) / 2
+    p = p - epsilon * grad_U(q, U_type, alpha, beta) / 2
     # Alternate full steps for position and momentum
     for (i in 1:L){
       # Make a full step for the position
       q = q + epsilon*p
       # Make a full step for the momentum, except at end of trajectory
-      if (i!=L){ p = p - epsilon * grad_U(q)}
+      if (i!=L){ p = p - epsilon * grad_U(q, U_type, alpha, beta)}
     }
     # Make a half step for momentum at the end.
-    p = p - epsilon * grad_U(q) / 2
+    p = p - epsilon * grad_U(q, U_type, alpha, beta) / 2
     ######################################################
     # Evaluate potential and kinetic energies at start and end of trajectory
-    current_U = U(current_q)
+    # switches depending on the value of U
+    if(U_type == "beta"){
+      current_U = -dbeta(current_q, alpha, beta, log = TRUE)
+      proposed_U = -dbeta(q, alpha, beta, log = TRUE)
+    } else if (U_type == "norm"){
+      current_U = -dnorm(current_q, mu, sigma, log = TRUE)
+      proposed_U = -dnorm(q, mu, sigma, log = TRUE)
+    }
     current_K = sum(current_p^2) / 2
-    proposed_U = U(q)
     proposed_K = sum(p^2) / 2
     # Accept or reject the state at end of trajectory, returning either
     # the position at the end of the trajectory or the initial position
@@ -87,10 +115,14 @@ server <- function(input, output) {
     
     D = numeric(length = input$bigN)
     for(i in 1:input$bigN){
-      D[i] = HMC(U, grad_U, 
+      D[i] = HMC(input$graph_type, 
                  epsilon = input$stepsize, 
                  L = input$num_steps, 
-                 current_q = cur_q)
+                 current_q = cur_q,
+                 alpha = input$alpha_prime, 
+                 beta = input$beta_prime,
+                 mu = input$mu_mean, 
+                 sigma = input$sigma_sd)
       cur_q = D[i]
     }
     D[500:input$bigN] # this is going to represent the chain
@@ -100,16 +132,20 @@ server <- function(input, output) {
     HMC_values()
   })
   
-  output$HMC_plot <- renderPlot({
+  output$HMC_plot = renderPlot({
     mean(D)
     p = seq(0,1, length=1000)
-    hist(HMC_values(), breaks = 'scott', freq = FALSE, xlim = c(0,1),
+    hist(HMC_values(), breaks = 'scott', freq = FALSE,
          border = "#ffffff",
          xlab = "Values of the Chain",
          main = "Histogram of the Densities of the Chain")
     n = 100
-    x = rbinom(n, 1, 0.5)
-    curve(dbeta(x,input$alpha_prime, input$beta_prime), col = 'red', add = TRUE)
+
+    if(input$graph_type == "beta"){
+      curve(dbeta(x,input$alpha_prime, input$beta_prime), col = 'red', add = TRUE)
+    } else if (input$graph_type == "norm"){
+      curve(dnorm(x,0, 1), col = 'red', add = TRUE)
+    }
   })
   
 }
