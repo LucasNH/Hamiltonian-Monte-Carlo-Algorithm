bd4a1e095c130617b5451ac988ca478e9899664c
Anna Ly <annaly@Annas-MacBook-Air.local>
2023-11-24 13:31:22 -0500

    separated the algorithm codes from the website to make it easier to understand/access

---
+++ HMC_algorithm.R
@@ -0,0 +1,64 @@
+grad_U = function(q, type, alpha = 50, beta = 50){
+  # U represents a distribution, and grad_U is the gradient of U.
+  # q: represents a sample from the distribution of U.
+  # type: the type of distribution the user is sampling from.
+  # alpha: the alpha parameter if the user samples from a beta dist.
+  # beta: the beta parameter if the user samples from a beta dist.
+  if(type == "beta"){
+    return(-(alpha/q - beta/(1-q)))
+  } else if (type == "norm"){
+    return(q)
+  }
+}
+
+HMC = function(n_samples, U_type, epsilon, L, current_q,
+               alpha = 50, beta = 50, mu = 0, sigma = 1){
+  # n_samples: the number of samples we want to obtain.
+  # U_type: the type of distribution we are sampling from. (For now, beta/normal)
+  # epsilon: the stepsize.
+  # L: number of steps per stepsize.
+  # current_q: the start value for the representative sample of our target dist.
+  # alpha, beta, mu, sigma: parameters of the U_type.
+  
+  q = current_q # q represents a sample from our target distribution.
+  D = numeric(length = n_samples) # D contains our n_samples
+  
+  for(j in 1:n_samples){
+    # Here, our kinetic energy is a standard normal distribution.
+    p = rnorm(n = 1, mean = 0, sd = 1) 
+    current_p = p
+    ######################################################
+    # Leapfrog Algorithm for solving the dynamical system
+    # Make a half step for momentum at the beginning
+    p = p - epsilon * grad_U(q, U_type, alpha, beta) / 2
+    # Alternate full steps for position and momentum
+    for (i in 1:L){
+      # Make a full step for the position
+      q = q + epsilon*p
+      # Make a full step for the momentum, except at end of trajectory
+      if (i!=L){ p = p - epsilon * grad_U(q, U_type, alpha, beta)}
+    }
+    # Make a half step for momentum at the end.
+    p = p - epsilon * grad_U(q, U_type, alpha, beta) / 2
+    ######################################################
+    # Evaluate potential and kinetic energies at start and end of trajectory
+    if(U_type == "beta"){
+      current_U = -dbeta(current_q, alpha, beta, log = TRUE)
+      proposed_U = -dbeta(q, alpha, beta, log = TRUE)
+    } else if (U_type == "norm"){
+      current_U = -dnorm(current_q, mu, sigma, log = TRUE)
+      proposed_U = -dnorm(q, mu, sigma, log = TRUE)
+    }
+    current_K = sum(current_p^2)/2
+    proposed_K = sum(p^2)/2
+    # This part corresponds with the acceptance-rejection method.
+    if (runif(1) < exp(current_U-proposed_U+current_K-proposed_K)){
+      D[j] = q
+      current_q = q # accept, need to change q value
+    } 
+    else {
+      D[j] = current_q
+    } 
+  }
+  return(D)
+}
--- app.R
+++ app.R
@@ -2,10 +2,14 @@ library(shiny)
 library(shinycssloaders)
 options(spinner.color="#9dabf5", spinner.color.background="#ffffff", type = 6)
 
+source("HMC_algorithm.R")
+
 # This is the front-end - user inputs and what are displayed.
 ui = fluidPage(
   
   titlePanel("Hamiltonian Monte Carlo"),
+  h4("Authors from MAT332: Mark Asuncion, Simran Bilkhu, Anna Ly, Lucas Noritomi-Hartwig"),
+  h4("Website is used for demonstration purposes only."),
 
   sidebarLayout(
     sidebarPanel(
@@ -50,76 +54,16 @@ ui = fluidPage(
 # This is the back-end.
 server = function(input, output) {
 
-  grad_U = function(q, type, alpha = 50, beta = 50){
-    # U represents a distribution, and grad_U is the gradient of U.
-    # q: represents a sample from the distribution of U.
-    # type: the type of distribution the user is sampling from.
-    # alpha: the alpha parameter if the user samples from a beta dist.
-    # beta: the beta parameter if the user samples from a beta dist.
-    if(type == "beta"){
-      return(-(alpha/q - beta/(1-q)))
-    } else if (type == "norm"){
-      return(q)
-    }
-  }
-  
-  HMC = function(U_type, epsilon, L, current_q,
-                 alpha = 50, beta = 50, mu = 0, sigma = 1){
-    # This function simulates the HMC once. You need to iterate over it for multiple samples.
-    # U_type: the type of distribution we are sampling from. Here, our kinetic energy 
-    # in this code, q represents a sample from the posterior distribution of interest...
-    q = current_q
-    # Here, our kinetic energy is a standard normal distribution.
-    p = rnorm(n = 1, mean = 0, sd = 1) 
-    current_p = p
-    ######################################################
-    # Leapfrog Algorithm for solving the dynamical system
-    # Make a half step for momentum at the beginning
-    p = p - epsilon * grad_U(q, U_type, alpha, beta) / 2
-    # Alternate full steps for position and momentum
-    for (i in 1:L){
-      # Make a full step for the position
-      q = q + epsilon*p
-      # Make a full step for the momentum, except at end of trajectory
-      if (i!=L){ p = p - epsilon * grad_U(q, U_type, alpha, beta)}
-    }
-    # Make a half step for momentum at the end.
-    p = p - epsilon * grad_U(q, U_type, alpha, beta) / 2
-    ######################################################
-    # Evaluate potential and kinetic energies at start and end of trajectory
-    if(U_type == "beta"){
-      current_U = -dbeta(current_q, alpha, beta, log = TRUE)
-      proposed_U = -dbeta(q, alpha, beta, log = TRUE)
-    } else if (U_type == "norm"){
-      current_U = -dnorm(current_q, mu, sigma, log = TRUE)
-      proposed_U = -dnorm(q, mu, sigma, log = TRUE)
-    }
-    current_K = sum(current_p^2)/2
-    proposed_K = sum(p^2)/2
-    # This part corresponds with the acceptance-rejection method.
-    if (runif(1) < exp(current_U-proposed_U+current_K-proposed_K))
-      { return (q) } # accept
-    else 
-      { return (current_q) } # reject
-  }
-  
-  
   HMC_values = reactive({
-    cur_q = input$current_position
-    
-    D = numeric(length = input$bigN)
-    for(i in 1:input$bigN){
-      D[i] = HMC(input$graph_type, 
-                 epsilon = input$stepsize, 
-                 L = input$num_steps, 
-                 current_q = cur_q,
-                 alpha = input$alpha_prime, 
-                 beta = input$beta_prime,
-                 mu = input$mu_mean, 
-                 sigma = input$sigma_sd)
-      cur_q = D[i]
-    }
-    D[500:input$bigN] # this is going to represent the chain
+    HMC(n_samples = input$bigN, 
+        U_type = input$graph_type, 
+        epsilon = input$stepsize, 
+        L = input$num_steps, 
+        current_q = input$current_position,
+        alpha = input$alpha_prime, 
+        beta = input$beta_prime,
+        mu = input$mu_mean, 
+        sigma = input$sigma_sd)
   })
   
   output$HMC_results = renderPrint({
@@ -127,7 +71,6 @@ server = function(input, output) {
   })
   
   output$HMC_plot = renderPlot({
-    mean(D)
     p = seq(0,1, length=1000)
     hist(HMC_values(), breaks = 'scott', freq = FALSE,
          border = "#ffffff",
